efc7af2f37b4aa8292a4bb6b56c8e22e21fc5847
Adrian <aphillpotts@gmail.com>
2021-11-04 12:45:07 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "network" {
   source  = "app.terraform.io/aphillpotts/network/google"
   version = "3.4.0"
   project_id = var.project
-  network_name = "adrian_network"
+  network_name = "adrian-network"
   # insert required variables here
   subnets = [
   {
